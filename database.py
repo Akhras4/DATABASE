b53d9c4e2ea9ca25bcc96fff3ac4f093ee5ddc86
akhras-eng <65845301+akhras-eng@users.noreply.github.com>
2020-07-16 16:26:31 +0200

    Add files via upload

---
+++ database.py
@@ -0,0 +1,10 @@
+import mysql.connector
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="0947600aaa",
+    database="user"
+)
+mycursor=mydb.cursor()
+mycursor.execute('CREATE TABLE INF (firstname VARCHAR(50), lastname VARCHAR(50),email VARCHAR(255),'
+                 'password VARCHAR(255),berth VARCHAR(255) ) ')
