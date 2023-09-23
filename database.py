4534bd61cd72b4ebe7cdbec7497f6feaaa25b0cc
akhras-eng <65845301+akhras-eng@users.noreply.github.com>
2023-09-23 12:47:13 +0200

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import mysql.connector
 mydb=mysql.connector.connect(
     host="localhost",
     user="root",
-    password="0947600aaa",
+    password="aaa",
     database="user"
 )
 mycursor=mydb.cursor()
